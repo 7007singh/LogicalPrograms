d978d9363477bfd7c8b4ff4c85ac5da928be99cf
7007singh <shw12021982@gmail.com>
2023-01-29 21:15:25 +0530

    [Shweta]Added: Print Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static void FindFibonacciSeries()
+        {
+            Console.WriteLine("Input number of terms to print fibonacci series");
+            int term = Convert.ToInt32(Console.ReadLine());
+            int firstNumber = 0; int secondNumber = 1;
+            Console.Write("{0} + {1} + ",firstNumber, secondNumber);
+            for(int i = 0; i <= term; i++)
+            {
+                int thirdNumber = firstNumber + secondNumber;
+                Console.Write(" {0} + ",thirdNumber);
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+            } 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Program number for execution");
+            Console.WriteLine("1:FibonacciSeries");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.Clear();
+                    FibonacciSeries.FindFibonacciSeries();
+                    break;   
+            }
+        }
+    }
+}
