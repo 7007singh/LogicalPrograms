7463f143979e3e147341b4d240107d5bcd62b0a5
7007singh <shw12021982@gmail.com>
2023-02-05 07:51:21 +0530

    [Shweta]Added: get minimum number of notes returned by vending machine

---
+++ LogicalPrograms/VendingMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class VendingMachine
+    {
+        public static void GetMinimumNotes()
+        {
+            Console.WriteLine("Please enter the amout ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] notes = { 1, 2, 5, 10, 50, 100, 500, 1000 };
+            int noteCount = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                if (notes[i] <= amount)
+                {
+                    amount = amount - notes[i];
+                    Console.Write(notes[i] + ",");
+                    i += 1;
+                    noteCount++;
+                }
+                if (amount == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nTotal no of minimum notes returned by vending machine:{0} ", noteCount);
+        }
+    }
+}
