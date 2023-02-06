947949c9bdf8f5d05b7be1d6ca14988b972f4e5c
7007singh <shw12021982@gmail.com>
2023-02-06 14:42:06 +0530

    [Shweta]Add: Finding coupon number by random method

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class CouponNumber
+    {
+        public static void FindDistinctCouponNumber()
+        {
+            char[] arr = new char[99];
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+
+            Random random = new Random();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                int randomCode = random.Next(chars.Length);
+                arr[i] = (chars[randomCode]);
+                Console.Write(arr[i]);
+
+                if (arr.Contains(arr[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    arr[i] = chars[randomCode];
+                }
+            }
+        }
+    }
+}
